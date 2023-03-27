01cf4cdb42da9d84d49b5c98973032b8feda0718
Andreas Söderlund <ciscoheat@gmail.com>
2023-03-28 00:11:33 +0200

    Added Timers component.

---
+++ src/lib/Timers.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import { derived, type Readable } from 'svelte/store';
+
+	export let submitting: Readable<boolean>;
+	export let delayed: Readable<boolean>;
+	export let timeout: Readable<boolean>;
+
+	let time = derived(
+		submitting,
+		($s, set) => {
+			let timer: Date | undefined = undefined;
+			const interval = setInterval(() => {
+				if (!$submitting) timer = undefined;
+				else if (!timer) timer = new Date();
+				let output = timer ? Date.now() - timer.getTime() + 'ms' : '';
+				output = output.padEnd(6);
+				set(output);
+			}, 1);
+
+			return () => clearInterval(interval);
+		},
+		'      '
+	);
+</script>
+
+<pre><span class:green={!$submitting}>Idle</span> -> <span class:green={$submitting}
+		>submitting</span
+	> -> <span class:green={$delayed}>delayed</span> -> <span class:green={$timeout}>timeout</span>
+{$time}  <span class:green={$submitting}>0 ms</span>          <span class:green={$delayed}
+		>500 ms</span
+	>     <span class:green={$timeout}>8000 ms</span>
+</pre>
+
+<style lang="scss">
+	pre {
+		span {
+			color: rgb(213, 48, 48);
+			&.green {
+				color: rgb(55, 200, 55);
+			}
+		}
+		font-size: 130%;
+		margin: 40px 0;
+	}
+</style>
--- src/routes/test/+page.md
+++ src/routes/test/+page.md
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Header from '../Header.svelte';
+	import Timers from '$lib/Timers.svelte';
 </script>
 
 <Header title="Title of this post" />
@@ -7,6 +8,8 @@
 ## Other typical markdown text
 
 Oh, you mean how you're supposed to act on a first date. Flux capacitor. Who's are these? He's fine, and he's completely unaware that anything happened. As far as he's concerned the trip was instantaneous. That's why Einstein's watch is exactly one minute behind mine. He skipped over that minute to instantly arrive at this moment in time. Come here, I'll show you how it works. First, you turn the time circuits on. This readout tell you where you're going, this one tells you where you are, this one tells you where you were. You imput the destination time on this keypad. Say, you wanna see the signing of the declaration of independence, or witness the birth or Christ. Here's a red-letter date in the history of science, November 5, 1955. Yes, of course, November 5, 1955. Nothing's coming to my mind.
+
+<Timers />
 
 ### Well done
 
