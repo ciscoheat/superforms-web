7bd5dcba6f2cc58c1504b9fcb8fa3c804d2cc4e3
Andreas Söderlund <ciscoheat@gmail.com>
2023-03-30 20:39:56 +0200

    Form error list fix.

--- src/routes/concepts/error-handling/Form.svelte
+++ src/routes/concepts/error-handling/Form.svelte
@@ -20,21 +20,9 @@
   use:enhance
 >
   {#if $allErrors}
-    <ul class="list">
+    <ul class="list m-0 p-0">
       {#each $allErrors as error}
-        <li>
-          <span class="text-red-500"
-            ><svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              ><path
-                fill="currentColor"
-                d="M11 15h2v2h-2v-2zm0-8h2v6h-2V7zm.99-5C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8s8 3.58 8 8s-3.58 8-8 8z"
-              /></svg
-            ></span
-          >
+        <li class="m-0 p-0">
           <span class="flex-auto"><b>{error.path}:</b> {error.message}</span>
         </li>
       {/each}
