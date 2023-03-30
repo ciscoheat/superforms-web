21491b7eb4ca2afb2afb13cd3954de12d8df3e74
Hunter Johnston <johnstonhuntera@gmail.com>
2023-03-29 22:07:06 -0400

    initial typography updates

--- src/lib/mdsvex/MarkdownLayout.svelte
+++ src/lib/mdsvex/MarkdownLayout.svelte
@@ -8,6 +8,6 @@
   export let fullWidth = false;
 </script>
 
-<div class="p-6 md:p-10 space-y-5 {fullWidth ? '' : 'max-w-4xl'} mx-auto">
+<div class="p-6 md:p-10 space-y-5 {fullWidth ? '' : 'max-w-4xl'} mx-auto prose prose-invert">
   <slot />
 </div>
