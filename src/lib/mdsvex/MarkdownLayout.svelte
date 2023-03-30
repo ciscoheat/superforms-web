8a17fa109ce64179b9910ddf6323acbfb8835bb5
Hunter Johnston <johnstonhuntera@gmail.com>
2023-03-29 21:39:41 -0400

    center content on larger screens

--- src/lib/mdsvex/MarkdownLayout.svelte
+++ src/lib/mdsvex/MarkdownLayout.svelte
@@ -8,6 +8,6 @@
   export let fullWidth = false;
 </script>
 
-<div class="p-6 md:p-10 space-y-5 {fullWidth ? '' : 'max-w-4xl'}">
+<div class="p-6 md:p-10 space-y-5 {fullWidth ? '' : 'max-w-4xl'} mx-auto">
   <slot />
 </div>
