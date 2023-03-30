14c6b755da405ed5b117ce869e00e99f0c7c419b
Andreas Söderlund <ciscoheat@gmail.com>
2023-03-30 17:05:45 +0200

    Status messages.

---
+++ src/routes/concepts/messages/Form.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+  import type { PageData } from './$types';
+  import { superForm } from 'sveltekit-superforms/client';
+  import { page } from '$app/stores';
+  import Debug from '$lib/Debug.svelte';
+
+  export function formData() {
+    return form;
+  }
+
+  export let data: PageData;
+  const { form, errors, enhance, tainted, message, constraints } = superForm(
+    data.form
+  );
+</script>
+
+<Debug label="$tainted store" status={false} open={true} data={$tainted} />
+
+<form
+  method="POST"
+  action={$page.url.pathname}
+  class="p-5 border-dashed bg-slate-900 border-2 border-primary-900 rounded-xl space-y-4"
+  use:enhance
+>
+  {#if $message}
+    <h3 class="rounded p-2 bg-green-700">{$message}</h3>
+  {/if}
+  <label class="label">
+    <span
+      >Name {#if $constraints.name?.required}*{/if}</span
+    >
+    <input class="input" type="text" name="name" bind:value={$form.name} />
+    {#if $errors.name}<span class="text-red-500">{$errors.name}</span>{/if}
+  </label>
+
+  <label class="label">
+    <span
+      >E-mail {#if $constraints.email?.required}*{/if}</span
+    >
+    <input class="input" type="text" name="email" bind:value={$form.email} />
+    {#if $errors.email}<span class="text-red-500">{$errors.email}</span>{/if}
+  </label>
+
+  <button type="submit" class="btn variant-filled">Submit</button>
+</form>
