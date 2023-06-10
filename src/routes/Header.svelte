81b126c54ede417a9eb9cc747d9a84016ade78b9
Andreas Söderlund <ciscoheat@gmail.com>
2023-06-10 19:30:41 +0200

    Svelte hack winner title

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -20,5 +20,6 @@
 
 <div class="flex flex-col items-center pb-4">
   <img src={logo} class="mt-1" width="130px" alt="Superforms logo" />
-  <h1>Superforms 💥</h1>
+  <h1 class="mb-4">Superforms 💥</h1>
+  <h4 class="mt-0">🏆 Svelte Hack 2023 winner - Best library! 🏆</h4>
 </div>
