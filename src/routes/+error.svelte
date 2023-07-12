4655a8f8552498f2197e0e9b2d913c7c14c1f2a4
Andreas Söderlund <ciscoheat@gmail.com>
2023-07-12 15:11:15 +0200

    Added error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+</script>
+
+<div>
+  <h1>{$page.status}</h1>
+  <h3>💥 {$page.error?.message} 💥</h3>
+</div>
+
+<style>
+  div {
+    height: 90vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
