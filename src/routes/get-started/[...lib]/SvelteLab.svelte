1286f2a35a8009e40e3b46d3fb93921fff89ea15
Andreas Söderlund <ciscoheat@gmail.com>
2024-03-13 09:06:05 +0100

    Fixed library links

--- src/routes/get-started/[...lib]/SvelteLab.svelte
+++ src/routes/get-started/[...lib]/SvelteLab.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
   export let lib: string;
-  if (lib == '@sinclair/typebox') lib = 'typebox';
-  else if (lib == '@vinejs/vine') lib = 'vine';
+  $: libName = lib == '@sinclair/typebox' ? 'typebox' : lib == '@vinejs/vine' ? 'vine' : lib;
 </script>
 
 <p>
   Alternatively, <a
-    href="https://sveltelab.dev/github.com/ciscoheat/superforms-examples/tree/tutorial-{lib}"
+    href="https://sveltelab.dev/github.com/ciscoheat/superforms-examples/tree/tutorial-{libName}"
     target="_blank">open the tutorial on SvelteLab</a> to follow along in the browser and copy the code
   from there.
 </p>
